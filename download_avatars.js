acb6d113acba44ed48f02c5c7ef10cf0ce8e773d
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 21:41:42 +0000

    Added github user and token to the request URL.

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'boomerandzapper';
+var GITHUB_TOKEN = 'fb2211804cddbe1adfa17b53f17c8260b01fe140';
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
-  console.log(url);
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
