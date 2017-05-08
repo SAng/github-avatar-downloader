f6ee9bf389b14a0e2ec26115e26e0befd3566136
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 21:17:29 +0000

    Completed URL endpoint variable conversion.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = 'https://api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
+  console.log(url);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
