f84b33776e4589783068c1b4207aaf7fdbee433c
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 23:39:32 +0000

    Completed downloadImageByUrl function and created an avatars directory to include downloaded avatars. Downloaded one avatar as a test.

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -25,3 +26,13 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(result.avatar_url);
   });
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
