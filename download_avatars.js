6a63e76d01219d63ce3bb54c71e074ca21035456
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 23:22:10 +0000

    Got rid of error by specifying user agent.

--- download_avatars.js
+++ download_avatars.js
@@ -6,10 +6,19 @@ var GITHUB_USER = 'boomerandzapper';
 var GITHUB_TOKEN = 'fb2211804cddbe1adfa17b53f17c8260b01fe140';
 
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'GitHub Avatar Downloader - Student Project'
+  }
+};
+
+
   console.log(requestURL);
-  request(requestURL, function(err, response, body) {
+  request(options, function(err, response, body) {
     if (err) throw err;
     console.log(body);
 
